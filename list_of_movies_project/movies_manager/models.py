d2f68481cde3da967dfc872fa713c09ffcdca24e
アンドリュー <bitweal12@gmail.com>
2024-02-28 14:48:06 +0200

    Models added
    added 3 models: Director, Actor, Movie

--- list_of_movies_project/movies_manager/models.py
+++ list_of_movies_project/movies_manager/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Director(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Actor(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=100)
+    year = models.IntegerField()
+    director = models.ForeignKey(Director, on_delete=models.CASCADE)
+    actors = models.ManyToManyField(Actor)
+
+    def __str__(self):
+        return self.title
